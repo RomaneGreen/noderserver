f2c49d4224679d79da8c7fe7a764d05bb72bfff0
Romane G <romane71193@gmail.com>
2019-09-08 15:05:16 -0400

    finished routes

--- server.js
+++ server.js
@@ -1,8 +1,43 @@
 
+let http = require('http'); 
+let fs = require('fs')
+  
+// Create a server object 
+http.createServer(function (req, res) { 
+      
+    // http header 
+    res.writeHead(200, {'Content-Type': 'text/html'});  
+      
+    let url = req.url; 
+    if(url ==='/') { 
 
-let http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(req.url);
-  res.end();
-}).listen(8080);
+      fs.readFile('index.html', function(err, data) {
+        res.write(data);
+         res.end();  
+      })
+    } 
+    else if(url ==='/about') { 
+
+      fs.readFile('about.html', function(err, data) {
+        res.write(data);
+         res.end();  
+      })
+    } 
+    else if(url ==='/contact-me') { 
+      fs.readFile('contact-me.html', function(err, data) {
+        res.write(data);
+        return res.end(); 
+      })
+    } 
+    else { 
+      fs.readFile('404.html', function(err, data) {
+        res.write(data);
+        return res.end();
+      })
+    } 
+}).listen(8080, function() { 
+      
+    // The server object listens on port 3000 
+    console.log("server start at port 8080"); 
+}); 
+    
